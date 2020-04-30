5844517c9626d00642b40740d4ea9e992adcc7bb
HMSConsulting <projetos@hmsconsulting.com.br>
2020-04-30 08:36:19 -0300

    first commit

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar-tarefa';
+
+@NgModule({
+  declarations: [ListarTarefaComponent],
+  imports: [CommonModule],
+  providers: [TarefaService],
+})
+export class TarefasModule {}
---
+++ src/app/tarefas/tarefas.routing.module.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { ListarTarefaComponent } from './listar-tarefa';
+export const TarefaRoutes: Routes = [
+  {
+    path: 'tarefas',
+    redirectTo: 'tarefas/listar',
+  },
+  {
+    path: 'tarefas/listar',
+    component: ListarTarefaComponent,
+  },
+];
